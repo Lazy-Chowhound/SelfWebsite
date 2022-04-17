60bcfa9f325286aabbc3f6138406b3df74624559
Lazy-Chowhound <1103036472@qq.com>
2022-04-17 14:28:58 +0800

    初始化spring boot

---
+++ website_java/src/main/java/szp/website/website_java/WebsiteJavaApplication.java
@@ -0,0 +1,16 @@
+package szp.website.website_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Nakano Miku
+ */
+@SpringBootApplication
+public class WebsiteJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteJavaApplication.class, args);
+    }
+
+}
---
+++ website_java/src/test/java/szp/website/website_java/WebsiteJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package szp.website.website_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
